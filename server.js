daa581a5c288e5aa3d65bd03d96436aa59fe3627
asadullah055 <asadullahahmed01@gmail.com>
2023-08-29 20:42:47 +0600

    second commit

--- server.js
+++ server.js
@@ -5,27 +5,32 @@ const server = HTTP.createServer((req, res) => {
   if (req.url === "/") {
     //If you request this url '/' then the response is  "This is Home Page".
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>This is Home Page</h1>");
+    res.write("<h1>This is Home Page</h1>");
+    res.end();
 
   } else if (req.url === "/about") {
     //If you request this url “/about” then the response is  “This is About Page”.
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>This is About Page</h1>");
+    res.write("<h1>This is About Page</h1>");
+    res.end();
 
   } else if (req.url === "/contact") {
     //If you request this url “/contact” then the response  is “This is Contact Page”.
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>This is Contact Page</h1>");
+    res.write("<h1>This is Contact Page</h1>");
+    res.end();
 
   } else if (req.url === "/file-write") {
     //If you request this url “/file-write” then fs.writeFile() method will create a file “demo.txt” and write the text “hello world” in this file.
     fs.writeFile("demo.txt", "hello world", (err) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/html" });
-        res.end("<h1>Error writing to file</h1>");
+        res.write("<h1>Error writing to file</h1>");
+        res.end();
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.end('<h1>File "demo.txt" created and text written</h1>');
+        res.write('<h1>File "demo.txt" created and text written</h1>');
+        res.end();
       }
     });
   }
